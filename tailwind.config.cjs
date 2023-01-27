31bdbef6f182362237e27ea8f097dc217a3f5c5d
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-01-27 16:30:12 -0500

    assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
+	daisyui: {
+		themes: [
+			{
+				stringTheme: {
+					primary: '#006AB7',
+
+					secondary: '#F92572',
+
+					neutral: '#003669',
+
+					info: '#009BE1',
+
+					success: '#15E883',
+
+					warning: '#FFEB84',
+
+					error: '#F92572'
+				}
+			}
+		]
 	},
-	plugins: []
+	plugins: [require('daisyui')]
 };
