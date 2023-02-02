5feb51c3a97cfa56662bdce2917362ba0641a533
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-02-02 14:17:35 -0500

    forgot verifyemail

--- src/lib/modals/login/VerifyEmail.svelte
+++ src/lib/modals/login/VerifyEmail.svelte
@@ -1,30 +1,37 @@
-<script lang="ts">
-	import ModalBase from '../ModalBase.svelte';
+<script lang='ts'>
+	import ModalBase from "../ModalBase.svelte";
 
-	import { activeModal } from '$lib/stores';
-	import CreateAccount from './CreateAccount.svelte';
-	import StyledButton from '$lib/components/StyledButton.svelte';
+	import { activeModal } from "$lib/stores";
+	import CreateAccount from "./CreateAccount.svelte";
+	import StyledButton from "$lib/components/StyledButton.svelte";
 
+
+	let email = "john@example.com"
+	
 	const back = () => {
 		$activeModal = CreateAccount;
-	};
+	}
 
-	const resend = () => {};
+	const handleResend = () => {
+
+	}
+	
 </script>
 
 <ModalBase>
-	<div class="flex flex-col justify-center items-center w-full h-full">
-		<img src="/assets/email_icon.svg" alt="Email" class="mb-12" />
+	<div class='flex flex-col justify-center items-center w-full h-full'>
+		<img src='/assets/email_icon.svg' alt='Email' class="mb-12" />
 
 		<h3 class="text-2xl font-bold">Check your email...</h3>
-		<p class="my-8">We’ve sent a verification email to john@email.com</p>
+		<p class="my-8">We’ve sent a verification email to {email}</p>
 		<div class="flex items-center mt-8">
 			<StyledButton className="btn-outline w-40 mr-6" action={back}>
 				<img src="/assets/button/left_chevron.svg" alt="<" class="mr-3" />
 				Go Back
 			</StyledButton>
-			<StyledButton className="w-40" action={resend}>Resend</StyledButton>
+			<StyledButton className="w-40" action={handleResend}>Resend</StyledButton>
 		</div>
-		<img src="/assets/string_logo.svg" alt="String" width="76px" height="18px" class="mt-12" />
+		<img src='/assets/string_logo.svg' alt='String' width="76px" height="18px" class="mt-12" />
+
 	</div>
 </ModalBase>
