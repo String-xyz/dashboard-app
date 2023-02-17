32b4030060672242f0bb14a8f6c7d5bf3d3d97dc
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-02-17 15:29:48 -0500

    Dashboard Create API Key redirect

--- src/lib/components/Dashboard/Card.svelte
+++ src/lib/components/Dashboard/Card.svelte
@@ -3,7 +3,7 @@
 	import { cards, CARD_NAME, type CardItem } from '$lib/constants/cards';
 	import { openLink } from '$lib/utils';
 
-	import { activeTab, getTabByName } from '$lib/stores';
+	import { activeTab, getTabByName, MenuItems } from '$lib/stores';
 	import StyledButton from '../StyledButton.svelte';
 	import { apiClient } from '$lib/services';
 	
@@ -14,21 +14,11 @@
 
 	cardActionMap.set(CARD_NAME.unlock_live, () => openLink(KYB_URL));
 	cardActionMap.set(CARD_NAME.explore_docs, () => openLink(DOCS_URL));
-	cardActionMap.set(CARD_NAME.manage_team, () => activeTab.set(getTabByName('Manage Team')));
-	cardActionMap.set(CARD_NAME.create_key, handleCreateApiKey);
+	cardActionMap.set(CARD_NAME.manage_team, () => activeTab.set(getTabByName(MenuItems.MANAGE_TEAM)));
+	cardActionMap.set(CARD_NAME.create_key, () => activeTab.set(getTabByName(MenuItems.MANAGE_KEYS)));
 
 	cards.forEach((card) => card.action = cardActionMap.get(card.id));
 	/* End Set cards actions */
-
-	async function handleCreateApiKey() {
-		try {
-			const apiKey = await apiClient.createApiKey();
-			// TODO: show success message 
-		} catch (error) {
-			console.error('--- error', error);
-			// TODO: show error message
-		}
-	}
 </script>
 
 <div class="card flex flex-col items-center">
