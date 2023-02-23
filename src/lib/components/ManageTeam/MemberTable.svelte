36a46750fb29350f171e63cb64b2d487bffe5cac
Frostbourne <85953565+frostbournesb@users.noreply.github.com>
2023-02-23 17:17:06 -0500

    Update member view in table (#19)

--- src/lib/components/ManageTeam/MemberTable.svelte
+++ src/lib/components/ManageTeam/MemberTable.svelte
@@ -6,6 +6,7 @@
 	import StyledButton from '../StyledButton.svelte';
 	import RoleDropdown from './RoleDropdown.svelte';
 	import { apiClient, authService } from '$lib/services';
+	import { formatDate } from '$lib/utils';
 
 	const handleResend = async (id: string) => {
 		try {
@@ -15,6 +16,11 @@
 			// TODO: Show notification error
 		}
 	}
+
+	const reactivateMember = () => {
+
+	}
+
 </script>
 <div class="members w-full">
 	<div class="flex justify-between py-4 font-bold">
@@ -40,27 +46,49 @@
 								<p class="mb-1 text-sm greyed font-semibold">{member.email}</p>
 								<p class="text-xs">Invitation sent!</p>
 							{:else}
-								<p class="mb-1 text-sm font-semibold">{member.email}</p>
-								<p class="text-xs greyed">{member.role} since {member.joinDate}</p>
+								{#if member.deactivatedAt}
+									<p class="text-sm font-semibold text-warning select-all">{member.name}</p>
+									<span class="text-xs greyed flex items-center mt-1 ">
+										<span class="select-all">{member.email}</span>
+										<span class="dot mx-2"></span>
+										<span class="select-all">Deactivated {formatDate(member.deactivatedAt)}</span>
+									</span>
+								{:else}
+									<p class="text-sm font-semibold select-all">{member.name}</p>
+									<span class="text-xs greyed flex items-center mt-1 ">
+										<span class="select-all">{member.email}</span>
+										<span class="dot mx-2"></span>
+										<span class="select-all">Joined {member.joinDate}</span>
+									</span>
+								{/if}
 							{/if}
 						</div>
 					</div>
-					<!-- If they are an admin or above, and it is not an admin trying to modify an admin -->
-					{#if authService.canModify($currentUser.role,  member.role)}
-						<div>
-							<RoleDropdown {member} isInvite={member.isInvite} />
-
-							{#if member.isInvite}
-								<StyledButton className="rounded-3xl w-32 h-8 ml-4" action={() => handleResend(member.id)}>
-									Resend
-									<img src="/assets/button/resend.svg" alt="resend" class="ml-2" />
-								</StyledButton>
+					{#if member.deactivatedAt}
+						<div class="flex justify-items-start items-center">
+							<p class="text-warning text-sm">Deactivated</p>
+							{#if authService.canView($currentUser.role, Role.ADMIN)}
+								<StyledButton className="btn-warning rounded-lg ml-8" action={reactivateMember}>Reactivate</StyledButton>
 							{/if}
 						</div>
 					{:else}
-						<p class="text-sm font-bold">
-							{member.role}
-						</p>
+						<!-- If they are an admin or above, and it is not an admin trying to modify an admin -->
+						{#if authService.canModify($currentUser.role, member.role)}
+							<div>
+								<RoleDropdown {member} isInvite={member.isInvite} />
+
+								{#if member.isInvite}
+									<StyledButton className="rounded-3xl w-32 h-8 ml-4" action={() => handleResend(member.id)}>
+										Resend
+										<img src="/assets/button/resend.svg" alt="resend" class="ml-2" />
+									</StyledButton>
+								{/if}
+							</div>
+						{:else}
+							<p class="text-sm font-bold">
+								{member.role}
+							</p>
+						{/if}
 					{/if}
 				{/if}
 			</div>
@@ -80,6 +108,14 @@
 	.members {
 		border: 1px solid #F2F2F2;
 		border-radius: 4px;
+	}
+
+	.dot {
+		border-radius: 50%;
+		width: 2px;
+		height: 2px;
+		background-color: #BEBCBA;
+		display: inline-block;
 	}
 
 </style>
