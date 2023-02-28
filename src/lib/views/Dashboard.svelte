907680852942ce56fcf27f7c60f29d53d85eb66e
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-02-28 13:35:13 -0500

    Toasts

--- src/lib/views/Dashboard.svelte
+++ src/lib/views/Dashboard.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { cards } from '$lib/constants/cards';
 	import Card from '$lib/components/Dashboard/Card.svelte';
+	import Toast from '$lib/components/Toast.svelte';
+	import { onMount } from 'svelte';
+
+	let showSuccessToast: () => void;
+
+	onMount(() => {
+		showSuccessToast();
+	})
 </script>
 
 <svelte:head>
@@ -18,6 +26,8 @@
 			{/each}
 		</div>
 	</div>
+
+	<Toast type="success" size="sm" bind:show={showSuccessToast}>This is a test </Toast>
 </div>
 
 <style>
