6c95bb2a8da99e87690527ae6939169a2fbe9a2f
frostbournesb <85953565+frostbournesb@users.noreply.github.com>
2023-04-17 12:36:03 -0400

    Remove toasts from invite modal

--- src/lib/modals/team/InviteTeammate.svelte
+++ src/lib/modals/team/InviteTeammate.svelte
@@ -5,7 +5,7 @@
 
 	import { Role } from "$lib/common/types";
 	import { validator } from "$lib/utils";
-	import { apiClient, teamService } from "$lib/services";
+	import { apiClient, ErrorCode, teamService } from "$lib/services";
 	import { inviteModalOpen, teamItems, toast } from "$lib/stores";
 
 	let dropdownOpen: boolean;
@@ -13,6 +13,8 @@
 
 	let emailInput: string;
 	let nameInput: string;
+
+	let emailAlreadyInUse = false;
 
 	$: isEmailValid = validator.isValidEmail(emailInput);
 	$: isNameValid = validator.isValidName(nameInput);
@@ -35,14 +37,14 @@
 
 			$toast.show("success", "Invite sent!");
 		} catch (e: any) {
-			console.error("code", e.code);
-
-			if (e.code === "CONFLICT") return $toast.show("error", "This email is already in use.");
-
-			$toast.show("error", "Oops, something went wrong. Please try again.");
+			if (e.code === ErrorCode.CONFLICT) {
+				emailAlreadyInUse = true;
+				setTimeout(() => {
+					emailAlreadyInUse = false;
+				}, 3000);
+				return;
+			}
 		}
-
-		$inviteModalOpen = false;
 	};
 
 	const handleKeyboard = (e: KeyboardEvent) => {
@@ -73,15 +75,22 @@
 
 			{#if $inviteModalOpen}
 				<StyledInput
-					className="mb-6 w-full"
+					className="w-full"
 					type="email"
 					label="Email address"
 					placeholder="test@string.xyz"
-					borderError={!isEmailValid && emailInput !== ""}
+					borderError={(!isEmailValid || emailAlreadyInUse) && emailInput !== ""}
 					bind:val={emailInput}
 					autofocus={true}
 					required
 				/>
+
+				{#if emailAlreadyInUse && emailInput !== ""}
+					<p class="text-warning text-sm mt-2 mb-4 mr-auto">This email is already in use.</p>
+				{:else}
+					<div class="mb-6" />
+				{/if}
+
 			{/if}
 
 			<div class="flex justify-between mb-20 w-full">
